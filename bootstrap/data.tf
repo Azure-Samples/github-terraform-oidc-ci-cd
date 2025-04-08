b6e3dffe04ed5b3d34bd1aadd8601b76842ecfa2
Jared Holgate <jaredholgate@microsoft.com>
2025-04-08 10:01:07 +0100

    fix: storage account name length restrictions

--- bootstrap/data.tf
+++ bootstrap/data.tf
@@ -4,3 +4,8 @@ data "azurerm_subscription" "current" {}
 data "github_organization" "this" {
   name = var.organization_name
 }
+
+module "regions" {
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "0.5.0"
+}
