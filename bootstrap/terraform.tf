2f2aa1938567fbca712d84893fcadcbafae81903
Jared Holgate <jaredholgate@microsoft.com>
2026-03-02 10:30:54 +0000

    Update azurerm provider version to 4.62

--- bootstrap/terraform.tf
+++ bootstrap/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.20"
+      version = "~> 4.62"
     }
     github = {
       source  = "integrations/github"
